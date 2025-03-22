466caf79572201330feab23cb402aadc8bde487e
Jaime Varas Cáceres <jv24040@edu.lu.lv>
2025-03-23 00:59:43 +0200

    Removed debugging files

--- jimmy_simulations_pkg/cmake-build-debug/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/jimmy/catkin_ws/src/jimmy_turtlebot_pkg/jimmy_simulations_pkg/cmake-build-debug/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
